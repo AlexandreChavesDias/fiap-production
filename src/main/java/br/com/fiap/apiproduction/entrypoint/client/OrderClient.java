368948ed2b064fa8b17a5a2481c121eac1645591
bit4save <bit4save@outlook.com>
2024-03-19 21:45:45 -0300

    Adicionando properties de prod e corrigindo o local

--- src/main/java/br/com/fiap/apiproduction/entrypoint/client/OrderClient.java
+++ src/main/java/br/com/fiap/apiproduction/entrypoint/client/OrderClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "orderClient", url = "http://localhost:8080/api/v1/orders")
+@FeignClient(name = "orderClient", url = "${ms-pedidos.api.url}")
 public interface OrderClient {
 
     @GetMapping
