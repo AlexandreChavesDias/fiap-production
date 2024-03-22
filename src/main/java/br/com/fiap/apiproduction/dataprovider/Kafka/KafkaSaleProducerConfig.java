873d2a6930177f1f8827f0b624e373884311e203
alexandre.dias <alexandrechavesdias@hotmail.com>
2024-03-22 14:56:20 -0300

    Refator chamada pra serviço externo e variaveis do kafka

--- src/main/java/br/com/fiap/apiproduction/dataprovider/Kafka/KafkaSaleProducerConfig.java
+++ src/main/java/br/com/fiap/apiproduction/dataprovider/Kafka/KafkaSaleProducerConfig.java
@@ -2,6 +2,7 @@ package br.com.fiap.apiproduction.dataprovider.Kafka;
 
 
 import br.com.fiap.apiproduction.dataprovider.Kafka.message.OrderMessage;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,7 +16,6 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
 import static org.apache.kafka.clients.CommonClientConfigs.SECURITY_PROTOCOL_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
 import static org.apache.kafka.common.config.SaslConfigs.SASL_JAAS_CONFIG;
@@ -28,16 +28,14 @@ public class KafkaSaleProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String CONFLUENT_CLOUD_BOOTSTRAP_SERVERS_VALUE;
 
-    @Value("${confluent.cloud.api.key}")
-    private String CONFLUENT_CLOUD_API_KEY;
+    @Value("${spring.kafka.properties.sasl.jaas.config}")
+    private String SASL_JAAS_CONFIG_VALUE;
 
-    @Value("${confluent.cloud.api.secret}")
-    private String CONFLUENT_CLOUD_API_SECRET;
     @Bean
     public ProducerFactory<String, OrderMessage> producerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(BOOTSTRAP_SERVERS_CONFIG, CONFLUENT_CLOUD_BOOTSTRAP_SERVERS_VALUE);
-        props.put(SASL_JAAS_CONFIG, "org.apache.kafka.common.security.plain.PlainLoginModule required username=\""+CONFLUENT_CLOUD_API_KEY+"\" password=\""+CONFLUENT_CLOUD_API_SECRET+"\";");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, CONFLUENT_CLOUD_BOOTSTRAP_SERVERS_VALUE);
+        props.put(SASL_JAAS_CONFIG, SASL_JAAS_CONFIG_VALUE);
         props.put(SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
         props.put(SASL_MECHANISM, "PLAIN");
         props.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
--- src/main/java/br/com/fiap/apiproduction/entrypoint/client/OrderClient.java
+++ src/main/java/br/com/fiap/apiproduction/entrypoint/client/OrderClient.java
@@ -2,8 +2,10 @@ package br.com.fiap.apiproduction.entrypoint.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Component
 @FeignClient(name = "orderClient", url = "${ms-pedidos.api.url}")
 public interface OrderClient {
 
